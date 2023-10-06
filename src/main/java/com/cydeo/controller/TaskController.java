f0977becb502f396a74d946d49b929643a871020
ZB <zeynepbstg@hotmail.com>
2023-10-06 10:34:38 -0400

    ProjectDtoConverter added to fix problem in creating new task

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -7,6 +7,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,13 +30,38 @@ public class TaskController {
          model.addAttribute("task", new TaskDTO());
          model.addAttribute("assignedEmployees", userService.findAll());
          model.addAttribute("projects", projectService.findAll());
+         model.addAttribute("tasks", taskService.findAll());
 
         return "/task/create";
     }
 
     @PostMapping("/create")
-    public String taskCreate(){
+    public String taskCreate(TaskDTO taskDTO){
 
+        taskService.save(taskDTO);
         return "redirect:/task/create";
     }
+
+    @GetMapping ("/update/{id}")
+    public String taskUpdate(@PathVariable ("id") Long id, Model model){
+        model.addAttribute("task", taskService.findById(id));
+        model.addAttribute("assignedEmployees", userService.findAll());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("tasks", taskService.findAll());
+
+        return "/task/update";
+    }
+//    @PostMapping ("/update/{taskId}")
+//    public String taskUpdate(@PathVariable ("taskId") Long id,TaskDTO task){
+//        task.setId(id);
+//        taskService.update(task);
+//        return "redirect:/task/create";
+//    }
+
+    @PostMapping("/update/{id}")
+    public String taskUpdate(TaskDTO task){
+        taskService.update(task);
+        return "redirect:/task/create";
+    }
+
 }
